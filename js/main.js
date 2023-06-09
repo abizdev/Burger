63aa9a50cfa46e1709fce91e4235600cd6756156
Abdulaziz <abizlancer@gmail.com>
2023-06-09 15:08:19 +0500

    part 2

--- js/main.js
+++ js/main.js
@@ -31,6 +31,7 @@ const arrProducts = []
 const cardBtnOpen = document.querySelector('.cart-btn')
 const cartBtnClose = document.querySelector('.cart-dropdown__close')
 const cardDropdown = document.querySelector('.cart-dropdown')
+const cartDropdownContent = document.querySelector('.cart-dropdown__content')
 
 cardList.forEach(function(card, cardKey) {
   const cardBtn = card.querySelector('.card-btn')
@@ -39,6 +40,7 @@ cardList.forEach(function(card, cardKey) {
   const cardCount = card.querySelector('.card-count')
   const countItemAmount = card.querySelector('.count-item__amount')
   const cardSymbol = card.querySelectorAll('.symbol-btn')
+  products[cardId]['imgSrc'] = card.querySelector('.card-img').src
   
   cardBtn.addEventListener('click', function(event) {
     event.preventDefault()
@@ -49,6 +51,11 @@ cardList.forEach(function(card, cardKey) {
     arrProducts.push(products[cardId])
     cardAmount.textContent = products[cardId].amount
     countItemAmount.textContent = products[cardId].amount
+    cartDropdownContent.innerHTML = ''
+    arrProducts.forEach(function(obj, keyObj) {
+      const item = cartItemTemplate(obj)
+      cartDropdownContent.append(item)
+    })
   })
   cardSymbol.forEach(function(btn, key) {
     btn.addEventListener('click', function(event) {
@@ -62,16 +69,23 @@ cardList.forEach(function(card, cardKey) {
       }
       cardAmount.textContent = products[cardId].amount
       countItemAmount.textContent = products[cardId].amount
+      const cartItemList = document.querySelectorAll('.cart-item')
       if(products[cardId].amount == 0) {
         cardBtn.classList.remove('active')
         cardAmount.classList.remove('active')
         cardCount.classList.remove('active')
+        arrProducts.forEach(function(item, key) {
+          if(item.amount == 0) {
+            arrProducts.splice(key, 1)
+          }
+        })
+        cartItemList.forEach(function(el, key) {
+          const idEl = el.getAttribute('id')
+          if(idEl == cardId) {
+            el.remove()
+          }
+        })
       }
-      arrProducts.forEach(function(item, key) {
-        if(item.amount == 0) {
-          arrProducts.splice(key, 1)
-        }
-      })
       console.log(arrProducts);
     })
   })
@@ -101,3 +115,37 @@ cartBtnClose.addEventListener('click', function(event) {
     cardDropdown.removeAttribute('style')
   }, 600);
 })
+
+function cartItemTemplate({name, amount, price, imgSrc}) {
+  const cartItem = document.createElement('div')
+        cartItem.classList.add('cart-item')
+        cartItem.setAttribute('id', name)
+  const img = document.createElement('img')
+        img.classList.add('cart-item__img')
+        img.src = imgSrc
+  const cartContent = document.createElement('div')
+        cartContent.classList.add('.cart-item__content')
+  const cartText = document.createElement('div')
+        cartText.classList.add('cart-item__content')
+  const cartCount = document.createElement('div')
+        cartCount.classList.add('cart-item__count', 'count-item')
+  const h5 = document.createElement('h5')
+        h5.textContent = name
+  const p = document.createElement('p')
+        p.textContent = `${price} сум`
+  const btnPlus = document.createElement('button')
+        btnPlus.classList.add('cart-item__symbol', 'symbol-btn')
+        btnPlus.textContent = '+'
+  const btnMinus = document.createElement('button')
+        btnMinus.classList.add('cart-item__symbol', 'symbol-btn')
+        btnMinus.textContent = '-'
+  const count = document.createElement('span')
+        count.classList.add('cart-item__amount', 'count-item__amount')
+        count.textContent = amount
+  
+  cartCount.append(btnMinus, count, btnPlus)
+  cartText.append(h5, p)
+  cartContent.append(cartText, cartCount)
+  cartItem.append(img, cartContent)
+  return cartItem
+}
